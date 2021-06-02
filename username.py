789d6908714e0b4f6a5444805b571e4e0a73c696
JH <jhiggz60@gmail.com>
2021-06-01 20:28:26 -0400

    Simple Python Exercises
    Simple Python Exercises for fun

---
+++ username.py
@@ -0,0 +1,9 @@
+username = 'Mary'
+password = 'swordfish'
+
+if username == 'Mary':
+    print('Hello, Mary')
+    if password == 'swordfish':
+            print('Access granted>')
+    else:
+        print('Incorrect password.')
